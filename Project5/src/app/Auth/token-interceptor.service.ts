076b3d58227f567e10aa526b8d81386d3ef0cc80
shyam@216143 <vennushyamkumar@gmail.com>
2022-09-30 18:43:47 +0530

    updated till injectors

---
+++ Project5/src/app/Auth/token-interceptor.service.ts
@@ -0,0 +1,36 @@
+import { HttpHandler, HttpRequest } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { UserServicesService } from '../services/user-services.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService {
+
+  constructor(private inject :Injector) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authservice=this.inject.get(UserServicesService)
+    let jwttoken=this.AddTokenHeader(req, authservice.getToken())
+    console.log(jwttoken)
+    return next.handle(jwttoken).pipe(
+      catchError(err => { 
+        if(err.data.status === 401) {
+          // need to implement logout methods
+          authservice.logout1()
+
+          // refresh token logic
+
+        }
+        return throwError("err occured at token intercept")
+      })
+    );
+  }
+  AddTokenHeader(request: HttpRequest<any>, token:any){
+  return request.clone({
+    headers:request.headers.set('Authorization', 'Bearer '+token)
+  });
+  }
+
+    
+}

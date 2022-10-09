46f5c3c11f72a5b2b9bac0748ee6c5d0fb6678e9
vennu shyam kumar <vennushyamkumar@gmail.com>
2022-10-09 14:24:36 +0530

    updated

--- SocialAppAngular/src/app/service/user.service.ts
+++ SocialAppAngular/src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -6,6 +6,7 @@ import { ResetPassword } from '../model/reset-password';
 import { UpdateUserEmail } from '../model/update-user-email';
 import { UpdateUserInfo } from '../model/update-user-info';
 import { UpdateUserPassword } from '../model/update-user-password';
+import { UserResponse } from '../model/user-response';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,10 @@ export class UserService {
   private host = environment.apiUrl;
 	// private jwtService = new JwtHelperService();
   constructor(private httpClient: HttpClient) { }
-
+  forgotPassword(email: string): Observable<any | HttpErrorResponse> {
+	const reqParams = new HttpParams().set('email', email);
+	return this.httpClient.post<any | HttpErrorResponse>(`${this.host}/forgot-password`, null, { params: reqParams });
+}
 
 
 
@@ -31,5 +35,17 @@ export class UserService {
   resetPassword(token: string, resetPassword: ResetPassword): Observable<any | HttpErrorResponse> {
 		return this.httpClient.post<any | HttpErrorResponse>(`${this.host}/reset-password/${token}`, resetPassword);
 	}
+
+	getUserSearchResult(key: string, page: number, size: number): Observable<UserResponse[]|any | HttpErrorResponse> {
+		const reqParams = new HttpParams().set('key', key).set('page', page).set('size', size);
+		return this.httpClient.get<UserResponse[] | HttpErrorResponse>(`${this.host}/users/search`, { params: reqParams });
+	}
+	followUser(userId: number): Observable<any | HttpErrorResponse> {
+		return this.httpClient.post<any | HttpErrorResponse>(`${this.host}/account/follow/${userId}`, null);
+	}
+	unfollowUser(userId: number): Observable<any | HttpErrorResponse> {
+		return this.httpClient.post<any | HttpErrorResponse>(`${this.host}/account/unfollow/${userId}`, null);
+	}
+
 
 }

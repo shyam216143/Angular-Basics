cc5ef9a9bec85dbda3f7f6c8094414bc3dd36260
shyam@216143 <vennushyamkumar@gmail.com>
2022-10-25 16:10:20 +0530

    udated

--- SocialAppAngular/src/app/component/home/home.component.ts
+++ SocialAppAngular/src/app/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {  Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import {
   combineLatest,
@@ -24,8 +24,8 @@ export class HomeComponent implements OnInit {
   @ViewChild('endOfChat')
   endOfChat!: ElementRef;
   userData!: any
-  selectedUserData!:User
-  users_data: User[]=[]
+  selectedUserData!: User
+  users_data: User[] = []
 
   private subscriptions: Subscription[] = [];
 
@@ -63,10 +63,41 @@ export class HomeComponent implements OnInit {
   }
 
   createChat(user: User) {
-this.selectedUserData=user
+    this.selectedUserData = user
   }
 
-  sendMessage() {
+  sendMessage(selectedUserData:any) {
+    const message = "shyam fbjkhfehguirhjuhgkojehguhg  nbfnjm jhbgvf mnjhbvf jhbjhnjkn unbkjnkj  nkjn njk nb implements"
+    const message1 = 'bfjkswddsd'
+    
+    function countWords(message: string) {
+      const arr = message.split(' ');
+
+      return arr
+
+    }
+    let arr = countWords(message)
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > 10) {
+        arr[i] = arr[i].slice(0, arr[i].length / 2) + " " + arr[i].slice(arr[i].length / 2);
+      }
+    }
+    let output = ''
+    for (let i of arr) {
+      output += i + " "
+    }
+
+
+    let ele = document.getElementById('chat-area')
+    if (ele != null){
+      ele.innerHTML += "<div style=\"width:100%;display:flex;flex-direction:row-reverse\"><p style=\"background-color:grey;max-width:40%;padding:0px 10px;border-radius:10px;\">" + output + message.length + "</p></div>"
+      ele.innerHTML += "<div style=\"width:100%;display:flex;flex-direction:row\"><img src=\"http://127.0.0.1:8000${selectedUserData.cover_photo}\"><p style=\"background-color:grey;max-width:40%;padding:0px 10px;border-radius:10px;\">" + output + message.length + "</p></div>"
+      let pixels= ele.clientHeight;
+     
+      ele.scrollBy(0,pixels);
+      
+    }   
+
 
   }
 
